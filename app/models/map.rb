30758cfac7ef5327c67d40d3c83e92db8d6f991e
David J. Lee <dlee@library.wisc.edu>
2013-03-05 16:18:12 -0600

    added compression to warped output
    Former-commit-id: b810ff59072d85c8d162a7fdf36caf5ce4c68888

--- app/models/map.rb
+++ app/models/map.rb
@@ -602,7 +602,8 @@ class Map < ActiveRecord::Base
     memory_limit =  (defined?(GDAL_MEMORY_LIMIT)) ? "-wm "+GDAL_MEMORY_LIMIT.to_s :  ""
 
     #check for colorinterop=pal ? -disnodata 255 or -dstalpha
-    command = "#{GDAL_PATH}gdalwarp #{memory_limit}  #{transform_option}  #{resample_option} -dstalpha #{mask_options} -s_srs 'EPSG:4326' #{temp_filename}.vrt #{dest_filename} -co TILED=YES"
+    #command = "#{GDAL_PATH}gdalwarp #{memory_limit}  #{transform_option}  #{resample_option} -dstalpha #{mask_options} -s_srs 'EPSG:4326' #{temp_filename}.vrt #{dest_filename} -co TILED=YES"
+    command = "#{GDAL_PATH}gdalwarp #{memory_limit}  #{transform_option}  #{resample_option} -dstalpha #{mask_options} -s_srs 'EPSG:4326' #{temp_filename}.vrt #{dest_filename} -co TILED=YES -co COMPRESS=DEFLATE"
     w_stdin, w_stdout, w_stderr = Open3::popen3(command)
     logger.info command
 
