76b69ca7b0e369296d0fc5c6dd85f1313eb83300
Ubuntu Mapstory <ubuntu@ip-10-62-66-255.ec2.internal>
2013-03-20 01:33:37 +0000

    oauth_consumers update
    Former-commit-id: bb0b21a0956be04b2f44432990390ed66aa36ba4

--- config/initializers/oauth_consumers.rb
+++ config/initializers/oauth_consumers.rb
@@ -28,15 +28,19 @@ OAUTH_CREDENTIALS={
 #       :site=>"http://hourfeed.com" # Remember to add a site for a generic OAuth site
 #     }
 #   },
+# this gem lacks oauth2 consumer support, these values do not make sense for
+# oauth 2.0.
     :mapstory=>{
-      :key=>"",
-      :secret=>"",
+      :key=>"c4c15df0710b16f01216",
+      :secret=>"fb74460f583399db7d4337b2d2401c6fe9ba1f93",
       :options=>{
         :site=>"http://mapstory.dev.opengeo.org",
-        :request_token_path=>"/oauth2/access_token",
-        :access_token_path=>"/oauth2/2/access_token",
-        :authorize_path=>"/oauth2/authorize",
-        :http_method=>:post
+        :token_url=>"/oauth2/access_token/",
+        :authorize_url=>"/oauth2/authorize/",
+     #   :request_token_path=>"/oauth2/access_token/",
+     #   :access_token_path=>"/oauth2/2/access_token/",
+     #   :authorize_path=>"/oauth2/authorize/",
+     #   :http_method=>:post
       }
     },
 #   :nu_bux=>{
