b33f601f6e1e9da028bf162d005719491226b5df
David J. Lee <dlee@library.wisc.edu>
2013-03-24 13:22:02 -0500

    update callback url
    Former-commit-id: 198e86f641e755367db643953252d001676620e1

--- app/controllers/mapstory_controller.rb
+++ app/controllers/mapstory_controller.rb
@@ -10,7 +10,7 @@ class MapstoryController < ApplicationController
 		@token = "none"
 
 		if params[:code]
-			@token = client.auth_code.get_token(params[:code], :redirect_uri => 'http://warper.mapstory.org/oauth_consumers/mapstory/callback')
+			@token = client.auth_code.get_token(params[:code], :redirect_uri => 'http://warper.mapstory.org/mapstory/callback')
 		end
 
 	end
@@ -18,7 +18,7 @@ class MapstoryController < ApplicationController
 	def authorize
 
 		client = OAuth2::Client.new('c4c15df0710b16f01216', 'fb74460f583399db7d4337b2d2401c6fe9ba1f93', :site => 'http://mapstory.dev.opengeo.org', :token_url => '/oauth2/access_token/', :authorize_url => '/oauth2/authorize/') 
-		@auth_url = client.auth_code.authorize_url(:redirect_uri => 'http://warper.mapstory.org/oauth_consumers/mapstory/callback')
+		@auth_url = client.auth_code.authorize_url(:redirect_uri => 'http://warper.mapstory.org/mapstory/callback')
 
 	end
 
