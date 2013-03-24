f618886f4d3fc630c4f2f01980c4af4e1633c4da
David J. Lee <dlee@library.wisc.edu>
2013-03-24 13:24:04 -0500

    update client init
    Former-commit-id: f87fa1d776485220dd65c6e2ea349ff013f13da9

--- app/controllers/mapstory_controller.rb
+++ app/controllers/mapstory_controller.rb
@@ -8,6 +8,7 @@ class MapstoryController < ApplicationController
 
 	def callback
 		@token = "none"
+		client = OAuth2::Client.new('c4c15df0710b16f01216', 'fb74460f583399db7d4337b2d2401c6fe9ba1f93', :site => 'http://mapstory.dev.opengeo.org', :token_url => '/oauth2/access_token/', :authorize_url => '/oauth2/authorize/') 
 
 		if params[:code]
 			@token = client.auth_code.get_token(params[:code], :redirect_uri => 'http://warper.mapstory.org/mapstory/callback')
