ef656b7186196e1ce497d466c0e26c1a6b0f3b6b
David J. Lee <dlee@library.wisc.edu>
2013-10-23 09:40:22 -0500

    Check cookie decoding
    Former-commit-id: 29ee636a5c7d87289093501845b6fefff65b31e1

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-Gaia-Map-Warper
+Map-Warper
 ===============
 
-This is a clone of the NYPL map warper created by Tim Waters, with a few changes for use in Gaia GPS. If you are interested in forking this project, I suggest you go with the original.
+This is a clone of the NYPL map warper created by Tim Waters, with changes for use in Gaia GPS and MapStory
+
+If you are interested in forking this project, I suggest you go with the original.
 
 This project is currently in use at warper.mapstory.org
+
+## Login
+The login code has been stripped out, and a cookie from MapStory is required
+to be a logged in user.
--- app/models/mapstory_cookie.rb
+++ app/models/mapstory_cookie.rb
@@ -1,13 +1,31 @@
 # Provides methods for handling the mapstory cookie.
 class MapstoryCookie
 
-# Pass in the contents of the cookie to verify and decode.
-# returns the username if success, false otherwise.
-def self.decode(data)
-	Rails.logger.info "Cookie Decode: Not doing anything at the moment."
+	# Pass in the contents of the cookie to verify and decode.
+	# returns the username if success, false otherwise.
+	def self.decode(data)
+		MAPSTORY_COOKIE_KEY
 
-	data
-end
+		name, hash = data.split(":")
+
+
+
+		Rails.logger.info "Cookie Decode: Not doing anything at the moment."
+		Rails.logger.info "Name is: #{name}"
+		Rails.logger.info "Hash is: #{hash}"
+
+		hash_check = OpenSSL::HMAC.hexdigest(OpenSSL::Digest::Digest.new('sha1'), MAPSTORY_COOKIE_KEY, name)
+
+		Rails.logger.info "Our generated hash is: #{hash_check}"
+
+		if hash_check == hash
+			Rails.logger.info "Hash verifies"
+		else
+			Rails.logger.info "Unable to verify hash!!!!"
+		end
+
+		data
+	end
 
 
 end
