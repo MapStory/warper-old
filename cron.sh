9594108d951ffba33ff0c92be30b1f6523dc3e2b
David J. Lee <david@lee.dj>
2014-08-18 21:22:38 -0500

    Source rvm

--- cron.sh
+++ cron.sh
@@ -2,5 +2,6 @@
 #
 # Run this via cron to clean up expired maps. Do this every 10 minutes or so.
 #
+source "$HOME/.rvm/scripts/rvm"
 cd /var/www/warper/
 RAILS_ENV=production rake remove:expired >> /var/www/warper/log/expired.log 2>&1
