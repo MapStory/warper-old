fd8aeab25b62265b017c193b9fa64a61f298fc55
David J. Lee <david@lee.dj>
2014-08-18 20:21:17 -0500

    update cron script

--- cron.sh
+++ cron.sh
@@ -2,4 +2,5 @@
 #
 # Run this via cron to clean up expired maps. Do this every 10 minutes or so.
 #
-RAILS_ENV=production rake remove:expired > expired.log 2>&1
+cd /var/www/warper/
+RAILS_ENV=production rake remove:expired > /var/www/warper/expired.log 2>&1
