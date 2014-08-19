563cc5513939c8ec45fc70b687b08a577e73756f
David J. Lee <david@lee.dj>
2014-08-18 19:59:24 -0500

    Fix typo

--- cron.sh
+++ cron.sh
@@ -2,4 +2,4 @@
 #
 # Run this via cron to clean up exipired maps. Do this every 10 minutes or so.
 #
-RAILS_ENV=production rake clean:expired > /dev/null 2>&1
+RAILS_ENV=production rake remove:expired > /dev/null 2>&1
